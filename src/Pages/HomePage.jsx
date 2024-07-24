2fd653795430657f3a2967fbaced751d6122df37
Iñigo Esteban <nack_z4@hotmail.com>
2024-07-24 09:53:50 +0200

    listo para deploy

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,37 +1,108 @@
+import React from 'react';
 import { Link, Route, Routes } from "react-router-dom";
+import redWine from '../images/vino-tinto.jpg';
+import whiteWine from '../images/vino-blanco.png';
+import roseWine from '../images/vino-rosado.jpg';
+import sparklingWine from '../images/vino-espumoso.jpg';
+import dessertWine from '../images/vino-postre.jpg';
+import portoWine from '../images/vino-oporto.jpg';
+
+
 
 function HomePage() {
   
   return (
     <div>
-
-<h1>CHOOSE OPTION</h1>
-
-{/* <Link to={`/wines/reds`}>
-<img className="red-wines-img" src="../src/images/vino-tinto.webp" alt="red wines" width={300}/>
-</Link>
-
-<Link to={`/wines/whites`}>
-<img className="white-wines-img" src="../src/images/vino-blanco.png" alt="white wines"width={300}/>
-</Link> */}
+  {/* <Link to={`/wines/reds`}>
+      <img className="red-wines-img" src={redWine} alt="red wines" width={100}/>
+  </Link>
+  <Link to={`/wines/reds`}>
+      <img className="white-wines-img" src={whiteWine} alt="white wines" width={100}/>
+  </Link>
+  <Link to={`/wines/reds`}>
+      <img className="rose-wines-img" src={roseWine} alt="rose wines" width={100}/>
+  </Link>
+  <Link to={`/wines/reds`}>
+      <img className="sparkling-wines-img" src={sparklingWine} alt="sparkling wines" width={100}/>
+  </Link>
+  <Link to={`/wines/reds`}>
+      <img className="dessert-wines-img" src={dessertWine} alt="dessert wines" width={100}/>
+  </Link>
+  <Link to={`/wines/reds`}>
+      <img className="porto-wines-img" src={portoWine} alt="porto wines" width={100}/>
+  </Link> */}
 
 <Link to={`/wines/reds`}>
-<button>reds</button>
+<button className="red-wines-img" style={{
+            backgroundImage: `url(${redWine})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '200px',
+            height: '200px',
+            color: 'white',
+            border: 'none',
+            margin: '10px',
+          }}></button>
 </Link>
 <Link to={`/wines/whites`}>
-<button>whites</button>
+<button className="white-wines-img" style={{
+            backgroundImage: `url(${whiteWine})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '200px',
+            height: '200px',
+            color: 'white',
+            border: 'none',
+            margin: '10px',
+          }}></button>
 </Link>
 <Link to={`/wines/rose`}>
-<button>rose</button>
+<button className="rose-wines-img" style={{
+            backgroundImage: `url(${roseWine})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '200px',
+            height: '200px',
+            color: 'white',
+            border: 'none',
+            margin: '10px',
+          }}></button>
 </Link>
 <Link to={`/wines/sparkling`}>
-<button>sparkling</button>
+<button className="sparkling-wines-img" style={{
+            backgroundImage: `url(${sparklingWine})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '200px',
+            height: '200px',
+            color: 'white',
+            border: 'none',
+            margin: '10px',
+          }}></button>
 </Link>
 <Link to={`/wines/dessert`}>
-<button>dessert</button>
+<button className="dessert-wines-img" style={{
+            backgroundImage: `url(${dessertWine})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '200px',
+            height: '200px',
+            color: 'white',
+            border: 'none',
+            margin: '10px',
+          }}></button>
 </Link>
 <Link to={`/wines/port`}>
-<button>port</button>
+<button className="porto-wines-img" style={{
+            backgroundImage: `url(${portoWine})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            width: '200px',
+            height: '200px',
+            color: 'white',
+            border: 'none',
+            margin: '10px',
+          }}></button>
 </Link>
 
 
